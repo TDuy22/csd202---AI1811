1d4ba09b48e4efe696c09c67d45d58d9619a057a
TDuy22 <155499542+TDuy22@users.noreply.github.com>
2024-01-14 19:12:52 +0700

    Create Q1.py

---
+++ Lab1/Q1.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class singlyLinkedList:
+    def __init__(self, head = None):
+        self.head = head
+
+    def addToHead(self, x):
+        newNode = Node(x)
+
+        if self.head == None:
+            self.head = newNode
+        else:
+            newNode.next = self.head
+            self.head = newNode.next
+
+    def addToTail(self, x):
+        newNode = Node(x)
+
+        currentNode = self.head
+        while currentNode.next:
+            currentNode = currentNode.next
+        currentNode.next = newNode
+
+if Node(None):
+    print("oke")
+
